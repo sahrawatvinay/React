7119ef3b04791d86bfbf31cb8a195322d0c3cc24
vinay <vinaysahrawat183a@gmail.com>
2024-09-02 10:33:36 +0530

    set loader

---
+++ social-media/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div class="spinner-border m-5" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  );
+};
+
+export default Loading;
--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,25 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/posts-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import Loading from "./Loading";
 
 const PostList = () => {
-    const { postList, addInitialPosts } = useContext(PostListData);
-    useEffect(() => {
-        fetch("https://dummyjson.com/posts")
-            .then((res) => res.json())
-            .then((data) => {
-                addInitialPosts(data.posts);
-            });
-    }, []);
-    return (
-        <>
-            {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-            {postList.map((post) => (
-                <Post key={post.id} post={post}></Post>
-            ))}
-        </>
-    );
+  const { postList, addInitialPosts } = useContext(PostListData);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      {loading && <Loading />}
+      {!loading && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      {!loading &&
+        postList.map((post) => <Post key={post.id} post={post}></Post>)}
+    </>
+  );
 };
 
 export default PostList;
